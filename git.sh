65dbbd09a3932348a8b3038029db8692c456ceb7
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-06-15 00:20:05 -0300

    add files

---
+++ git.sh
@@ -0,0 +1,7 @@
+#echo "# obd2_flt" >> README.md
+git init
+git add README.md
+git commit -m "OBD2 Scanner ELM327 Bluettoth sync App Flutter "
+git branch -M main
+git remote add origin https://github.com/siliconvalleyar-oss/obd2_flt.git
+git push -u origin main
